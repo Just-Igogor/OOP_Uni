d0e7227899cfd1ff70d2dfcefa53f6a82046b906
Смелый <149298913+Just-Igogor@users.noreply.github.com>
2024-09-18 09:55:44 +0300

    Update main.cpp
    Change main file

--- Laba_2/main.cpp
+++ Laba_2/main.cpp
@@ -1,11 +1,16 @@
+/*ЮФУ, ИКТИБ, МОП ЭВМ
+Объектно-ориентированное программирование
+КТбо2-6, Смелов Игорь Александрович
+Лабораторная работа 2
+19.09.2024*/
+
 #define __CRTDBG_MAP_ALLOC
 #include "Sorting.h"
 
 int main() {
     setlocale(LC_ALL, "ru");
-    // Запуск процесса выбора и сортировки
-    SortManager::process();
-
+    SortManager manager;
+    manager.process();
     _CrtDumpMemoryLeaks();
     return 0;
 }
