7abd3f8f45ea8a3a0fc701e97a4c3045a245a62e
Смелый <149298913+Just-Igogor@users.noreply.github.com>
2024-10-01 13:12:03 +0300

    Update main.cpp
    Добавил код реализующего файла Лаба 4

--- Laba_4/main.cpp
+++ Laba_4/main.cpp
@@ -1,1 +1,40 @@
+/*ЮФУ, ИКТИБ, МОП ЭВМ
+Объектно-ориентированное программирование
+КТбо2-6, Смелов Игорь Александрович
+Лабораторная работа 4
+03.10.2024*/
+#include "Sorting.h"
 
+int main() {
+    setlocale(LC_ALL, "ru");
+    std::string filename = "cinema.txt";
+
+    try {
+        int choice;
+        std::cout << "Выберите пункт программы:\n1. Перечень названий фильмов без повторов\n2. Средний процент заполнения зала для фильма\nВаш выбор: ";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            // Выполнение пункта a)
+            displayUniqueMovieTitles(filename);
+        }
+        else if (choice == 2) {
+            // Выполнение пункта b)
+            std::vector<Cinema> cinemas = readCinemaData(filename);
+
+            std::string movie_name;
+            std::cout << "Введите название фильма: ";
+            std::cin >> movie_name;
+
+            displayMovieOccupancy(cinemas, movie_name);
+        }
+        else {
+            std::cout << "Некорректный выбор!" << std::endl;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
